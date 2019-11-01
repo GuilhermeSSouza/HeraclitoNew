40094431f603a323916a8a58235ae17fe45c8700
Guilherme Santos <guilthys@gmail.com>
2019-11-01 13:33:35 -0300

    adicionando "modal provador"

---
+++ src/main/java/com/unipmpa/edu/controller/NovoExercicioBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unipmpa.edu.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ApplicationScoped;
+
+/**
+ *
+ * @author heraclitoserver
+ */
+@ManagedBean
+@ApplicationScoped
+public class NovoExercicioBean {
+
+    /**
+     * Creates a new instance of NovoExercicio
+     */
+    public NovoExercicioBean() {
+    }
+    
+}
